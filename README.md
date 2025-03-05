d549ce6309ec1ee04e27e9c4bde5c42962362f94
Joshua Lee <jminlee24+1@outlook.com>
2025-03-05 18:00:55 -0500

    json added

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 file reader is a modified version of the sample given in the metavision SDK
 decodes the raw into an array of events rather than a csv, most of the logic is still the same
 
-TODO: Make code pretty and usable
+###TODO: Make code pretty and usable
 
 required libraries:
 Eigen 3.4
 OptimLib - Eigen - Header only 
+nlohmann/json
 
+How to make nlohmann/json findable in usr/local for cmake
+
+`
+clone nlohmann/json into usr/local/include
+cmake -B build -DCMAKE_INSTALL_PREFIX=usr/local -DJSON_BuildTests=OFF
+cmake --build build --target install
+`
 
 MAKE SURE YOU COMPILE IN RELEASE MODE, OR ELSE EIGEN WILL BE LITERALLY 1000x SLOWER
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "contrastmax.hpp"
 #include "filereader.hpp"
 #include <Eigen/Dense>
+#include <fstream>
 #include <nlohmann/json.hpp>
 
 #include <chrono>
@@ -9,9 +10,12 @@
 
 using json = nlohmann::json;
 
-int main(int argc, char *argv[]) {
-  FileReader::filedata_t fileData =
-      FileReader::read_file("../data/zybo_recording1.raw");
+int main() {
+
+  std::ifstream f("../config.json");
+  json config = json::parse(f);
+
+  FileReader::filedata_t fileData = FileReader::read_file(config["filepath"]);
 
   int width = fileData.metadata.width;
   int height = fileData.metadata.height;
@@ -21,35 +25,46 @@ int main(int argc, char *argv[]) {
       fileData.events, fileData.events[0].timestamp,
       fileData.events[0].timestamp + 1000000);
 
-  ContrastMax::image_t prev_image =
-      ContrastMax::create_image(fileData.events, width, height);
-
-  ContrastMax::write_image(prev_image, "prev.pgm");
-
   auto start = std::chrono::high_resolution_clock::now();
 
+  // check if we want to run the maximization with a gaussian blur
+  // with sigma = 2 * sqrt(variance)
+
+  // runmax is a function pointer
+  Eigen::Vector3d (*runmax)(FileReader::filedata_t);
   Eigen::Vector3d val;
-  if (argc > 1 && std::string(argv[1]) == std::string("--blur")) {
-    std::cout << "Running blur optimization" << std::endl;
-    val = ContrastMax::maximize(fileData);
+  if (config["blur"]) {
+    std::cout << "Running the optimization with a gaussian blur" << std::endl;
+    runmax = ContrastMax::maximize_blur;
   } else {
-    std::cout << "Running standard optimization" << std::endl;
-    val = ContrastMax::maximize(fileData);
+    std::cout << "Running the standard optimization" << std::endl;
+    runmax = ContrastMax::maximize;
   }
+  val = runmax(fileData);
 
   auto end = std::chrono::high_resolution_clock::now();
   std::chrono::duration<double> elapsed = end - start;
 
-  std::vector<ContrastMax::event_t> warped_events =
-      ContrastMax::warp_events(fileData.events, val);
+  // if we want to show some images of the warped events and the previous image
+  if (config["generate_images"]) {
+    std::cout << "Generate previous image at prev.pgm, and the warped image at "
+                 "warped.pgm \n";
 
-  ContrastMax::image_t image =
-      ContrastMax::create_image(warped_events, width, height);
-  ContrastMax::write_image(image, "warped.pgm");
+    ContrastMax::image_t prev_image =
+        ContrastMax::create_image(fileData.events, width, height);
 
-  std::cout << "Single pass: " << elapsed.count() << std::endl;
+    std::vector<ContrastMax::event_t> warped_events =
+        ContrastMax::warp_events(fileData.events, val);
+
+    ContrastMax::image_t image =
+        ContrastMax::create_image(warped_events, width, height);
+
+    ContrastMax::write_image(prev_image, "prev.pgm");
+    ContrastMax::write_image(image, "warped.pgm");
+  }
+  std::cout << "Single pass: " << elapsed.count() << " s" << std::endl;
   std::cout << val << std::endl;
-  std::cout << "Run on " << fileData.events.size() << " events." << std::endl;
+  std::cout << "Run on " << fileData.events.size() << " events" << std::endl;
 
   return 0;
 }
